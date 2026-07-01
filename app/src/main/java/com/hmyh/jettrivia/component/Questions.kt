0683f3871bd24a0453a99a1c66b013673a5739e8
hsumyatyehtet <hsumyatyehtet@gmail.com>
2026-07-01 15:50:05 +0630

    UI implementation

--- app/src/main/java/com/hmyh/jettrivia/component/Questions.kt
+++ app/src/main/java/com/hmyh/jettrivia/component/Questions.kt
@@ -1,25 +1,120 @@
 package com.hmyh.jettrivia.component
 
 import android.util.Log
+import android.view.View
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hmyh.jettrivia.screens.QuestionViewModel
+import com.hmyh.jettrivia.util.AppColors
 
 @Composable
-fun Question(viewModel: QuestionViewModel){
+fun Question(viewModel: QuestionViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
 
-    if (viewModel.data.value.loading == true){
+    if (viewModel.data.value.loading == true) {
 
         CircularProgressIndicator()
 
         Log.d("Loading", "Question....loading...")
-    }
-    else{
+    } else {
         Log.d("Loading", "Question loading stop")
 
         questions?.forEach { questionItem ->
-            Log.d("Result","Questions ${questionItem.question}")
+            Log.d("Result", "Questions ${questionItem.question}")
         }
     }
+}
+
+@Preview
+@Composable
+fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f),0f)
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp),
+        color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+            DrawDottedLine(
+                pathEffect = pathEffect,
+                Modifier.padding(vertical = 12.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect,modifier: Modifier) {
+
+    Canvas(modifier = modifier
+        .fillMaxWidth()
+        .height(1.dp)) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+
+    }
+
+}
+
+
+@Composable
+fun QuestionTracker(counter: Int = 10, outOff: Int = 100) {
+
+    Text(text = buildAnnotatedString {
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+            withStyle(
+                style = SpanStyle(
+                    color = AppColors.mLightGray,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp
+                )
+            ) {
+                append("Question $counter/")
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 14.sp
+                    )
+                ) {
+                    append("$outOff")
+                }
+            }
+        }
+    })
+
 }
